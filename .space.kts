ef252c336cfa0198c83aeaf6cf5bf8b0759c7ba7
Mohammed Imran <mohammed.kachnawa@warbabank.com>
2022-10-16 08:48:56 +0000

    update self hosted worker

--- .space.kts
+++ .space.kts
@@ -12,7 +12,7 @@ job("Run me on git push and at 8 AM UTC") {
     }
     
     requirements {
-        workerPool = WorkerPools.SPACE_CLOUD // not necessary if workerType is specified
+        workerPool = WorkerPools.SELF_HOSTED // not necessary if workerType is specified
     }
     
     host("Run script") {
